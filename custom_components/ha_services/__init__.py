e0d5227d191a9992b8963fded42259f5fb4b3c24
clipman <clipman@naver.com>
2022-04-17 23:22:30 +0900

    Update __init__.py

--- custom_components/ha_services/__init__.py
+++ custom_components/ha_services/__init__.py
@@ -27,7 +27,7 @@ CONF_ACCESS_TOKEN = 'access_token'
 
 
 def base_config_schema(config: dict = {}) -> dict:
-    """Return a shcema configuration dict for ST Updater."""
+    """Return a shcema configuration dict for HA-Services."""
     if not config:
         config = {
             CONF_APP_URL: "",
@@ -48,60 +48,6 @@ def config_combined() -> dict:
     return base
 
 CONFIG_SCHEMA = vol.Schema({DOMAIN: config_combined()}, extra=vol.ALLOW_EXTRA)
-
-async def async_setup(hass, config):
-
-    if DOMAIN not in config:
-        return True
-
-    app_url      = config[DOMAIN][CONF_APP_URL]
-    app_id       = config[DOMAIN][CONF_APP_ID]
-    access_token = config[DOMAIN][CONF_ACCESS_TOKEN]
-
-    session = async_get_clientsession(hass)
-
-    registerList = await getRegisteredHADeviceList(session, app_url, app_id, access_token)
-
-    async def event_listener(event):
-
-        newState = event.data['new_state']
-
-        if newState is None or newState.state in (STATE_UNKNOWN, '') or newState.entity_id not in registerList:
-            return None
-
-        id  = newState.entity_id
-        url = app_url + app_id + "/update?access_token=" + access_token + "&entity_id=" + newState.entity_id + "&value=" + newState.state
-
-        try:
-            url += "&unit=" + newState.as_dict()['attributes']['unit_of_measurement']
-        except:
-            url = url
-
-        try:
-            attr = json.dumps(newState.as_dict().get('attributes'))
-            attr = base64.b64encode(attr.encode()).decode()
-        except:
-            attr = ""
-
-        try:
-            oldstate = event.data['old_state'].state
-        except:
-            oldstate = ""
-
-        url += "&attr="+attr+"&old="+oldstate
-
-        response = await session.get(url)
-
-
-    async def do_refresh(call):
-        nonlocal registerList
-        registerList = await getRegisteredHADeviceList(session, app_url, app_id, access_token)
-
-
-    hass.bus.async_listen(EVENT_STATE_CHANGED, event_listener)
-    hass.services.async_register(DOMAIN, "refresh", do_refresh)
-
-    return True
 
 
 async def getRegisteredHADeviceList(session, app_url, app_id, access_token):
@@ -124,23 +70,24 @@ async def async_setup_entry(hass, config_entry):
     access_token = config_entry.data[CONF_ACCESS_TOKEN]
 
     session = async_get_clientsession(hass)
-
     registerList = await getRegisteredHADeviceList(session, app_url, app_id, access_token)
 
     async def event_listener(event):
-
         newState = event.data['new_state']
-
         if newState is None or newState.state in (STATE_UNKNOWN, '') or newState.entity_id not in registerList:
             return None
 
         id  = newState.entity_id
-        url = app_url + app_id + "/update?access_token=" + access_token + "&entity_id=" + newState.entity_id + "&value=" + newState.state
-
-        try:
-            url += "&unit=" + newState.as_dict()['attributes']['unit_of_measurement']
-        except:
-            url = url
+        entity_type = id.split('.')[0]
+        if(entity_type=='sensor' or entity_type=='binary_sensor') {
+            url = app_url + app_id + "/sensor?access_token=" + access_token + "&entity_id=" + id + "&value=" + newState.state
+            try:
+                url += "&unit=" + newState.as_dict()['attributes']['unit_of_measurement']
+            except:
+                url = url
+        } else {
+            url = app_url + app_id + "/device?access_token=" + access_token + "&entity_id=" + id + "&value=" + newState.state
+        }
 
         try:
             attr = json.dumps(newState.as_dict().get('attributes'))
@@ -154,7 +101,6 @@ async def async_setup_entry(hass, config_entry):
             oldstate = ""
 
         url += "&attr="+attr+"&old="+oldstate
-
         response = await session.get(url)
 
 
